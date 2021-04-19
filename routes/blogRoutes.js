0d1965c29d78f20be3bbf73333c1280d8eba9572
nikkbh <nikhilbhutani111@yahoo.com>
2021-04-19 14:55:50 +0530

    Updated code

---
+++ routes/blogRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const blogController = require('../controllers/blogControler')
+
+const router = express.Router();
+
+// Blog routes
+
+router.get('/', blogController.blog_index);
+
+router.post('/', blogController.blog_create_post);
+
+router.get('/create', blogController.blog_create_get);
+
+router.get('/:id', blogController.blog_details);
+
+router.delete('/:id', blogController.blog_delete);
+
+module.exports = router;
